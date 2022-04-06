c636cf65091ead38a8e1ee642f4e57c26497d53a
ericlr1 <ericluque2003@gmail.com>
2022-04-06 11:32:12 +0200

    Fondo pocho

--- Project_6_Solution/Project_6_Solution/Source/Globals.h
+++ Project_6_Solution/Project_6_Solution/Source/Globals.h
@@ -20,8 +20,8 @@ enum class update_status
 typedef unsigned int uint;
 
 // Window Configuration -----------
-#define SCREEN_WIDTH 384
-#define SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 200
+#define SCREEN_HEIGHT 325
 
 #define SCREEN_SIZE 3
 #define WIN_FULLSCREEN 0
