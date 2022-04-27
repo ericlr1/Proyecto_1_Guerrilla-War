d7bd93992efcba8eafbbd1c99bbab045364285e7
MarcelSunyer <marcelsunyercaldu@gmail.com>
2022-04-27 11:19:56 +0200

    Anim

--- Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
+++ Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
@@ -862,7 +862,7 @@ void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
 			}
 		if (c1->type == Collider::Type::BODY && c2->type == Collider::Type::WATER)
 		{
-			
+			currentAnimation2 = &water;
 		}
 
 
