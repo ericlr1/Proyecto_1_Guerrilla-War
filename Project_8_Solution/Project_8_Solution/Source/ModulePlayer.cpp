4e905920ac1e9b893e531b5adb0f8f64e94aeca0
MarcelSunyer <marcelsunyercaldu@gmail.com>
2022-05-03 10:55:17 +0200

    Cositas
    as

--- Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
+++ Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
@@ -1111,11 +1111,7 @@ void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
 		}
 
 		//Collider victoria
-		if (c1->type == Collider::Type::BODY && c2->type == Collider::Type::WIN)
-		{
-			App->fade->FadeToBlack((Module*)App->sceneLevel_1, (Module*)App->sceneIntro, 100);
-			destroyed = true;
-		}
+		
 
 		//Matar al pj con balas del enemigo
 		if (c1->type == Collider::Type::BODY && c2->type == Collider::Type::ENEMY_SHOT)
