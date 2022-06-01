50d42c1e2a249c61d06b475e9874a4f17ebe2258
ericlr1 <ericluque2003@gmail.com>
2022-06-01 19:55:16 +0200

    h for hostage

--- Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
+++ Project_8_Solution/Project_8_Solution/Source/ModulePlayer.cpp
@@ -1678,6 +1678,7 @@ Update_Status ModulePlayer::PostUpdate()
 			App->fonts->BlitText(10, 200, scoreFont, "F9.GREEN.SOILDER");
 			App->fonts->BlitText(10, 210, scoreFont, "F10.TRIPLESHOT");
 			App->fonts->BlitText(10, 220, scoreFont, "F11.RED.SOILDER");
+			App->fonts->BlitText(10, 230, scoreFont, "H.HOSTAGE");
 		}
 
 		App->fonts->BlitText(10, 290, scoreFont, "COINS");
